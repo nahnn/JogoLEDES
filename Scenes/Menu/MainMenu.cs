3720dc8e3aa22a6400f9e8cd60cb9eca5470b3fb
koddart <jose_sousa@ufms.br>
2025-03-15 15:56:09 -0400

    Revert "Teste de Art Char"
    This reverts commit 35cfe6db15ceeece0951370625c6663c15003877.

--- Scenes/Menu/MainMenu.cs
+++ Scenes/Menu/MainMenu.cs
@@ -5,14 +5,13 @@ public partial class MainMenu : Control {
 	public override void _Ready(){
 		GetNode<Button>("%Start").Pressed += _Play;
 		GetNode<Button>("%Quit").Pressed += _QuitGame;
-				GetNode<Button>("%Quit").Pressed += _QuitGame;
 	}
 
 
 	private void _Play()
 	{
 		TransitionManager transitionManager = GetNode<TransitionManager>("/root/TransitionManager");
-		transitionManager._FadeToScene("res://Scenes/Player/newPlayer.tscn");
+		transitionManager._FadeToScene("res://Scenes/Level1/level_1.tscn");
 	}
 
 	private void _QuitGame(){
